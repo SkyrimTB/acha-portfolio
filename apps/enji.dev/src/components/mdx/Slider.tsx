67313b362ac231e99680cd95d137e17a54a27d15
acha <Guidscope_Z@yahoo.com>
2024-07-29 02:30:27 -0700

    hide skill&tool / add parkin / add slider components

---
+++ apps/enji.dev/src/components/mdx/Slider.tsx
@@ -0,0 +1,40 @@
+// components/Slider.tsx
+import clsx from 'clsx';
+import NextImage from 'next/image';
+import React from 'react';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface ImageSliderProps {
+  images: { src: string; width?: number; height?: number }[];
+}
+
+const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider {...settings}>
+      {images.map((image, index) => (
+        <div key={index} className={clsx('relative')}>
+          <NextImage
+            src={image.src}
+            width={image.width || 800} // default width
+            height={image.height || 600} // default height
+            className={clsx('rounded-lg')}
+            alt={`Slide ${index}`}
+          />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default ImageSlider;
